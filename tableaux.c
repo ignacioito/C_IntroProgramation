8839fedb085ba02359d6e35fcf061d35f040d49f
ignacio <ignacioito@gmail.com>
2018-09-17 15:08:20 -0400

    Array
    Tablaux

---
+++ tableaux.c
@@ -0,0 +1,22 @@
+
+
+//
+//
+//Author Ignacio
+// Les tableaux
+//
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(int argc, char const *argv[])
+{
+	char nom [] = {'G', 'E', 'R', 'T', 'R', 'U', 'D', 'E'};
+	printf("%c", nom[3]);
+	printf("%c", nom[5]);
+	printf("%c", nom[3]);
+	printf("%c", nom[5]);
+	printf("\n");
+
+}
