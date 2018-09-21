660147fddfb9b4b553fff97d1f7e917314f9b380
ignacio <ignacioito@gmail.com>
2018-09-21 10:04:40 -0400

    mise à jour

--- matrice1.c
+++ matrice1.c
@@ -2,6 +2,16 @@
 //Autheur Ignacio Ito
 //
 //
+/*
+#1 Remplir une matrice 10 par 10, chaqwue case vaut le numero de la rangée
+000
+111
+222
+
+#2 Remplir une matrice 10 par 10, chaqwue case vaut le numero de la colonne
+
+#3 Remplir une matrice 10 par 10, chaqwue case vaut 0
+*/
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -14,14 +24,19 @@ void imprimerMatrice(int nbRangees, int nbColonnes, int maMatrice[nbRangees][nbC
 		}
 		printf("\n");
 }
+
+
 }
 
 
-int main()
-{
+
+
+int main(){
 	int maMatrice[3][3] = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
 	imprimerMatrice(3, 3, maMatrice);
 	
+
+
 }
 	
 
